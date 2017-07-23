6c5f3b89692496c0504703f4b806d52a309fa1ee
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 10:46:58 +0300

    Added the the relationships for the brand.rb

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,3 +1,12 @@
 class Brand <ActiveRecord::Base
-  belongs_to_many(:stores)
+  has_and_belongs_to_many(:stores)
+  validates :name, presence: true
+  # validates_uniqueness_of :name
+  before_save(:capitalize_name)
+
+  private
+
+  define_method(:capitalize_name) do
+    self.name = self.name.capitalize
+  end
 end
