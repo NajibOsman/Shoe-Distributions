c38590cec2128efc909c5b8614aa2a9b4e69e06a
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 11:00:07 +0300

    included the callback for capitalizing the fast letter of the name

--- lib/brand.rb
+++ lib/brand.rb
@@ -1,12 +1,12 @@
 class Brand <ActiveRecord::Base
   has_and_belongs_to_many(:stores)
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates_uniqueness_of :name
   before_save(:capitalize_name)
 
   private
 
-  #   define_method(:capitalize_name) do
-  #     self.name = self.name.capitalize
-  #   end
+  define_method(:capitalize_name) do
+    self.name = self.name.capitalize
+  end
 end
