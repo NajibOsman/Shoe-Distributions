7de6eedb6a7daef7bbe1b5dd2a7087d699ae23b4
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 10:43:36 +0300

    spec for capitalizing the first letter of namein brand_spec.rb file

--- spec/brand_spec.rb
+++ spec/brand_spec.rb
@@ -1,1 +1,19 @@
+require 'spec_helper'
 
+describe(Brand) do
+  # spec for capitalizing the first letter of name
+  describe('#capitalize_name') do
+    it "capitalizes inputted name" do
+      test_brand = Brand.create({:name => "nike"})
+      expect(test_brand.name).to(eq('Nike'))
+    end
+  end
+
+  # describe('#uniqueness') do
+  #   it "disallows creation of the same Brand name more than once" do
+  #     test_brand = Brand.create({:name => "Nike"})
+  #     test_brand2 = Brand.new({:name => "Nike"})
+  #     expect(test_brand2.save).to(eq(false))
+  #   end
+  # end
+end
