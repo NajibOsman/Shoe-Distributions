b8e255a3c8df76703d815214b751fb55b60b1f71
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 11:15:43 +0300

    required all gems in app.rb and routed the homepage

--- app.rb
+++ app.rb
@@ -4,6 +4,86 @@ require('pry')
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+#Route to the index
 get("/") do
   erb(:index)
 end
+
+# get('/stores') do
+#   @all_stores = Store.all()
+#   erb(:stores)
+# end
+
+# post('/stores/create_new_store') do
+#   name_input = params[:name_input]
+#   @new_store = Store.create({:name => name_input})
+#   if @new_store.save
+#     redirect('/stores')
+#   else
+#     erb(:error)
+#   end
+# end
+
+# get('/stores/store/:id') do
+#   @current_store = Store.find(params[:id])
+#   @current_store_brands = @current_store.brands
+#   @all_brands = Brand.all
+#   erb(:store)
+# end
+
+# patch('/store/:id/add_brand') do
+#   @current_store = Store.find(params[:id])
+#   @added_brand = Brand.find(params[:added_brand])
+#   if @current_store.brand_unique_per_store(@added_brand)
+#     @current_store.brands.push(@added_brand)
+#     redirect("/stores/store/#{@current_store.id}")
+#   else
+#     erb(:error)
+#   end
+# end
+
+# patch('/store/:id/update_name') do
+#   @current_store = Store.find(params[:id])
+#   new_name = params[:new_name]
+#   if @current_store.update({:name => new_name})
+#     redirect("/stores/store/#{@current_store.id}")
+#   else
+#     erb(:error)
+#   end
+# end
+
+# get('/brands') do
+#   @all_brands = Brand.all()
+#   erb(:brands)
+# end
+
+# post('/brands/create_new_brand') do
+#   name_input = params[:name_input]
+#   @new_brand = Brand.create({:name => name_input})
+#   if @new_brand.save
+#     redirect('/brands')
+#   else
+#     erb(:error)
+#   end
+# end
+
+# get('/brands/brand/:id') do
+#   @current_brand = Brand.find(params[:id])
+#   erb(:brand)
+# end
+
+# patch('/brand/:id/update_name') do
+#   @current_brand = Brand.find(params[:id])
+#   new_name = params[:new_name]
+#   if @current_brand.update({:name => new_name})
+#     redirect("/brands/brand/#{@current_brand.id}")
+#   else
+#     erb(:error)
+#   end
+# end
+
+# delete('/store/:id/delete') do
+#   @current_store = Store.find(params[:id])
+#   @current_store.destroy
+#   redirect('/stores')
+# end
