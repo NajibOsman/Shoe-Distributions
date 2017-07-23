8c88683d82d6b96006aefb1d5c5b77ea44fa6a6a
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 11:06:55 +0300

    Included  the validation of the name in the store.rb

--- lib/store.rb
+++ lib/store.rb
@@ -1,6 +1,6 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:brands)
-  # validates :name, presence: true
+  validates :name, presence: true
   # validates_uniqueness_of :name
   before_save(:capitalize_name)
 
