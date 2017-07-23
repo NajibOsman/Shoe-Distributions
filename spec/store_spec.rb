94a252072aa19daf340fbb5a0e70cda38c2fbb7d
NajibOsman <machidydemajka38@gmail.com>
2017-07-23 10:42:07 +0300

    spec for adding anew brand to a storein the store_spec.rb file

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -1,1 +1,45 @@
-store_spec.rb
+require 'spec_helper'
+
+describe(Store) do
+  # spec for adding anew brand to a store
+  describe('#brand') do
+    it "adds a brand to a store" do
+      test_store = Store.create({:name => 'Portland Store'})
+      test_brand = Brand.create({:name => 'Awesome Possum'})
+      test_store.brands.push(test_brand)
+      expect(test_store.brands).to(eq([test_brand]))
+    end
+  end
+
+  # describe('#store') do
+  #   it "disallows blank name with validation" do
+  #     test_store = Store.new({:name => ""})
+  #     expect(test_store.save).to(eq(false))
+  #   end
+  # end
+
+  # describe('#capitalize_name') do
+  #   it "capitalizes inputted name" do
+  #     test_store = Store.create({:name => "nike outlet"})
+  #     expect(test_store.name).to(eq('Nike outlet'))
+  #   end
+  # end
+
+  # describe('#uniqueness') do
+  #   it "disallows creation of the same Brand name more than once" do
+  #     test_brand = Brand.create({:name => "Nike"})
+  #     test_brand2 = Brand.new({:name => "Nike"})
+  #     expect(test_brand2.save).to(eq(false))
+  #   end
+  # end
+
+  # describe('#brand_unique_per_store') do
+  #   it "disallows adding same brand twice to the same store" do
+  #     test_store = Store.create({:name => "Store"})
+  #     test_brand = Brand.create({:name => "Brand"})
+  #     test_store.brands.push(test_brand)
+  #     expect(test_store.brand_unique_per_store(test_brand)).to(eq(false))
+  #   end
+  # end
+
+end
